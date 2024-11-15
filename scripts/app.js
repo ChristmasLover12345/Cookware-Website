0148446814594969fc908a987534817db9d6db1f
ChristmasLover12345 <madnes0205@gmail.com>
2024-11-15 13:51:28 -0800

    progress on slide menu

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+
+
+let menuButton = document.querySelector('menuButton');
+let closeButton = document.querySelector('closeMenu');
+let slideMenu = document.getElementById('slideMenu');
+
+
+menuButton.addEventListener('click') 
+{
+
+
+    
+}
