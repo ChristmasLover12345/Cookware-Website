b68e34178490a73954e83a56e5a61ad8566d87a2
ChristmasLover12345 <madnes0205@gmail.com>
2024-11-15 15:02:54 -0800

    responsivenes

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,22 @@
 
 
-let menuButton = document.querySelector('menuButton');
-let closeButton = document.querySelector('closeMenu');
-let slideMenu = document.getElementById('slideMenu');
+let slide = document.querySelector('.slideRight')
 
 
-menuButton.addEventListener('click') 
-{
+document.addEventListener('DOMContentLoaded', () => {
+    let menuButton = document.querySelector('.menuButton');
+    let closeButton = document.querySelector('.closeMenu');
+    let slideMenu = document.getElementById('slideMenu');
 
+   
+    menuButton.addEventListener('click', () => {
+        slideMenu.classList.add('Active');
+        
+    });
 
     
-}
+    closeButton.addEventListener('click', () => {
+        slideMenu.classList.remove('Active');
+    });
+});
+
